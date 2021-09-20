dae4e5096e85ceaf29640c027bb15a28f9653e3a
alvsan1 <alvsanchez@hotmail.com>
2021-09-20 00:24:09 -0300

    Form Material ui ok V1

---
+++ app/javascript/components/AxionHeaders.js
@@ -0,0 +1,14 @@
+
+import axios from 'axios'
+
+const setAxiosHeaders = () => {
+    const csrfToken = document.querySelector('[name=csrf-token]')
+    if (!csrfToken) {
+        return
+    }
+    const csrfTokenContent = csrfToken.content
+    csrfTokenContent &&
+        (axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfTokenContent)
+}
+
+export default setAxiosHeaders
--- app/javascript/components/MaterialUserForm.js
+++ app/javascript/components/MaterialUserForm.js
@@ -1,23 +1,49 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, FormProvider } from "react-hook-form";
+import axios from 'axios'
+import setAxiosHeaders from './AxionHeaders'
 import Container from '@material-ui/core/Container';
+import Input from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 export default function App() {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
-   
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => {console.log(data);
+        //data.preventDefault();
+        setAxiosHeaders();
+        axios
+            .post('/usuarios', data )
+            .then(response => {
+                const todoItem = response.data
+                this.props.createTodoItem(todoItem)
+                //this.props.clearErrors()
+            })
+            .catch(error => {
+                this.props.handleErrors(error)
+            });
+        //data.target.reset()
+      };
+
   return (
     <Container>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName")} />
-      <select {...register("gender")}>
-        <option value="female">female</option>
-        <option value="male">male</option>
-        <option value="other">other</option>
-      </select>
-      <input type="submit" />
-    </form>
+    <FormProvider onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="label_nombre">Nombre</label>
+      <Input id="nombre" {...register('nombre', { required: true, maxLength: 30 })} />
+      <label htmlFor="label_mail">eMail</label>
+      <Input id="email" {...register('mail', { required: true, maxLength: 30 })} />
+      {errors.nombre && errors.nombre.type === "required" && <span>This is required</span>}
+      {errors.nombre && errors.nombre.type === "maxLength" && <span>Max length exceeded</span> }
+      
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleSubmit(onSubmit)}
+      >
+        SUBMIT
+      </Button>
+
+
+    </FormProvider>
     </Container>
   );
 }
-
